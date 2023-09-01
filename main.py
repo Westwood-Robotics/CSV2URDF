b9458ea9d322f9b584031c0562572a52cb83ac57
Xiaoguang Zhang <hawkrybruin@gmail.com>
2023-09-01 21:31:34 +0800

    --

---
+++ main.py
@@ -0,0 +1,15 @@
+__author__ = "X. Zhang, Westwood Robotics Corporation"
+__email__ = "info@westwoodrobotics.io"
+__copyright__ = "Copyright 2023 Westwood Robotics"
+__date__ = "Sep. 01, 2023"
+
+__version__ = "0.0.1"
+__status__ = "beta"
+
+from csv2urdf import CSV2URDF
+
+robot_name = "BRUCE_V1.4_DUMMY_Sim_simplified"
+csv_path = "files/"+robot_name+".csv"
+urdf_path = "files/"+robot_name+".urdf"
+
+CSV2URDF(robot_name, csv_path, urdf_path)
